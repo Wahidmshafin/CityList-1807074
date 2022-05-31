0d6174a3564b78fa1b7ccc4049336d09b33b324f
Shafin <mahmood1807074@stud.kuet.ac.bd>
2022-05-31 22:25:10 +0600

    Test methods are implemented.

--- app/src/main/java/com/example/citylist/MainActivity.java
+++ app/src/main/java/com/example/citylist/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
